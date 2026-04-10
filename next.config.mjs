eb62c4e0c1705915275ad8f5b01a37c505363de7
Masa <102147397+masa-alaya@users.noreply.github.com>
2026-04-11 01:34:43 +0300

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,15 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export', // هاد السطر هو اللي بيخلي الموقع يشتغل كـ static site
   typescript: {
     ignoreBuildErrors: true,
   },
   images: {
     unoptimized: true,
   },
+  // ملاحظة: إذا اسم المستودع تبعك هو dudububu ضيف السطرين اللي تحت
+  basePath: '/dudububu',
+  assetPrefix: '/dudububu',
 }
 
 export default nextConfig
